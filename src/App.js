302cc7d11a6d0fa8cd5d2874f6d6ac6e6e4a88c9
SasiPrasa <sasprabandara@gmail.com>
2021-11-15 16:41:00 +0530

    Chat engine installed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="565c434b-dba5-4d0f-81de-eb8b0d602dd5"
+            userName="Sasindu"
+            userSecret="123123"
+        />
+    );
+}
+
+export default App;
